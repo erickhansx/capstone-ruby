5df0f7fe7dc08093b51017deeb612b47a156e7c5
solly <solobarine@gmail.com>
2023-01-17 14:10:43 +0100

    Fix Linters

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -4,11 +4,11 @@ require_relative '../src/genre'
 describe MusicAlbum do
   context 'Test MusicAlbum class and its methods' do
     before(:each) do
-      author = { id: 572, first_name: "Ndorrh",  last_name: "oswald beu", items: [] }
-      source = {id: 76, name: "Online shop", items: []}
-      publish_date = "10/10/2011"
+      author = { id: 572, first_name: 'Ndorrh', last_name: 'oswald beu', items: [] }
+      source = {id: 76, name: 'Online shop', items: []}
+      publish_date = '10/10/2011'
       on_spotify = true
-      label = { id: 119, title: "Gift", color: "Yellow", items: [] }
+      label = { id: 119, title: 'Gift', color: 'Yellow', items: [] }
       @genre = Genre.new('Action')
       @music = MusicAlbum.new(@genre, author, source, label, publish_date, on_spotify)
     end
@@ -25,6 +25,6 @@ describe MusicAlbum do
     @genre.add_item(@music)
     len = @genre.items.length
     expect(len).to eql 2
-  end
+    end
   end
 end
--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -3,11 +3,11 @@ require_relative '../src/music_album'
 describe MusicAlbum do
   context 'Test MusicAlbum class and its methods' do
     before(:each) do
-      genre = {id: 384, name: "Action", items: [] }
-      author = { id: 572, first_name: "Ndorrh",  last_name: "oswald beu", items: [] }
-      source = {id: 76, name: "Online shop", items: []}
-      label = { id: 119, title: "Gift", color: "Yellow", items: [] }
-      publish_date = "10/10/2011"
+      genre = { id: 384, name: 'Action', items: [] }
+      author = { id: 572, first_name: 'Ndorrh',  last_name: 'oswald beu', items: [] }
+      source = { id: 76, name: 'Online shop', items: []}
+      label = { id: 119, title: 'Gift', color: 'Yellow', items: [] }
+      publish_date = '10/10/2011'
       on_spotify = true
       @music = MusicAlbum.new(genre, author, source, label, publish_date, on_spotify)
     end
@@ -26,6 +26,6 @@ describe MusicAlbum do
 
   it 'Return true if on spotify is true and Publish date is older than 10 years' do
     expect(@music.move_to_archive).to eql true
-  end
+    end
   end
 end

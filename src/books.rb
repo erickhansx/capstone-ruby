0da0ee6a7fa80b5815489fee4e2751798be95dbd
ndorrh <ndorrh@gmail.com>
2023-01-17 06:39:39 +0100

    Add private method can_be_archive?() in Book class

--- src/books.rb
+++ src/books.rb
@@ -8,4 +8,10 @@ class Book < Item
     @publisher = publisher
     @cover_state = cover_state
   end
+
+  private
+
+  def can_be_archived?()
+    super || @cover_state == 'bad' ? true : false
+  end
 end

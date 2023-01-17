6378222b99057043c4ecdcf465bad54347d8ad52
solly <solobarine@gmail.com>
2023-01-17 17:30:32 +0100

    Create add music album method

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
 require_relative './src/modules/read_write_file'
+require_relative './src/author'
+require_relative './src/genre'
+require_relative './src/book'
+require_relative './src/music_album'
+require_relative './src/game'
+require_relative './src/label'
 require_relative './src/modules/get_user_input'
 require_relative './src/modules/read_file'
 
@@ -21,19 +27,28 @@ class App
     puts 'Follow the Prompt to Add a new music Album'
     new_genre = Genre.new(@user_input.genre(thing))
     @lood_files[:genres] << new_genre unless @lood_files[:genres].include?(new_genre)
-    print_dash(174)
+    print_dash(130)
     new_author = @user_input.author
     author = Author.new(new_author[:f_name], new_author[:l_name])
     @lood_files[:authors] << author unless @lood_files[:authors].include?(author)
-    print_dash(174)
+    print_dash(130)
     new_label = @user_input.label
     label = Label.new(new_label[:title], new_label[:color])
     @lood_files[:labels] << label unless @lood_files[:labels].include?(label)
-    print_dash(174)
+    print_dash(130)
     [new_genre, author, label]
   end
 
-
+def add_music_album
+    arg = get_attributes('Music')
+    new_date = @user_input.publish_date('On spotify')
+    music = MusicAlbum.new(arg[0], arg[1], arg[3], arg[2], new_date[:date], new_date[:boolean])
+    new_music = @user_input.parse_data(music)
+    @lood_files[:music_albums] << new_music unless @lood_files[:music_albums].include?(new_music)
+    print_dash(130)
+    puts '                                                     music album added successfully                                    '
+    print_dash(130)
+  end
 
 
 

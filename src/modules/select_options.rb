e90584a133c2a95025aa9361551da9c43ea9d068
erickhansx <erickhansx@gmail.com>
2023-01-17 09:43:51 -0600

    Modified UI

--- src/modules/select_options.rb
+++ src/modules/select_options.rb
@@ -5,25 +5,19 @@ def select_options(option, app)
   when 2
     app.list_all_music_albums
   when 3
-    app.list_all_movies
-  when 4
     app.list_all_games
-  when 5
+  when 4
     app.list_all_genres
-  when 6
+  when 5
     app.list_all_labels
-  when 7
+  when 6
     app.list_all_authors
-  when 8
-    app.list_all_sources
-  when 9
+  when 7
     app.add_book
-  when 12
-    app.add_music_album
-  when 10
-    app.add_movie
-  when 11
+  when 8
     app.add_game
+  when 9
+    app.add_music_album
   when 0
     puts 'You Exited the App'
   else

5a9386ed281c9efde6a06d54bf875679bda88ad9
ndorrh <ndorrh@gmail.com>
2023-01-17 14:44:16 +0100

    Fix linter errors

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -5,7 +5,7 @@ describe MusicAlbum do
   context 'Test MusicAlbum class and its methods' do
     before(:each) do
       author = { id: 572, first_name: 'Ndorrh', last_name: 'oswald beu', items: [] }
-      source = {id: 76, name: 'Online shop', items: []}
+      source = { id: 76, name: 'Online shop', items: [] }
       publish_date = '10/10/2011'
       on_spotify = true
       label = { id: 119, title: 'Gift', color: 'Yellow', items: [] }
@@ -14,17 +14,17 @@ describe MusicAlbum do
     end
 
     it 'Returns the instance of class' do
-    expect(@genre).to be_instance_of Genre
-  end
+      expect(@genre).to be_instance_of Genre
+    end
 
-     it 'check properties' do
-    expect(@genre.name).to eql 'Action'
-  end
+    it 'check properties' do
+      expect(@genre.name).to eql 'Action'
+    end
 
-  it 'Add item to array' do
-    @genre.add_item(@music)
-    len = @genre.items.length
-    expect(len).to eql 2
+    it 'Add item to array' do
+      @genre.add_item(@music)
+      len = @genre.items.length
+      expect(len).to eql 2
     end
   end
 end

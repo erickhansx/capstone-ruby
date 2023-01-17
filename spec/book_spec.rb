3f053f18c245c6768355838d39b5aa214549c7d1
ndorrh <ndorrh@gmail.com>
2023-01-17 14:15:09 +0100

    fix linter errors

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -3,13 +3,13 @@ require_relative '../src/book'
 describe Book do
   context 'Test Book class and its methods' do
     before(:each) do
-      genre = {id: 384, name: "Comedy", items: [] }
-      author = { id: 572, first_name: "Ndorrh",  last_name: "oswald beu", items: [] }
-      source = {id: 76, name: "Friend", items: []}
-      label = { id: 119, title: "New", color: "Green", items: [] }
-      publish_date = "10/102015"
-      cover_state = "good"
-      publisher = "oswald house"
+      genre = { id: 384, name: 'Comedy', items: [] }
+      author = { id: 572, first_name: 'Ndorrh', last_name: 'oswald beu', items: [] }
+      source = { id: 76, name: 'Friend', items: [] }
+      label = { id: 119, title: 'New', color: 'Green', items: [] }
+      publish_date = '10/102015'
+      cover_state = 'good'
+      publisher = 'oswald house'
       @book = Book.new(genre, author, source, label, publish_date, publisher, cover_state)
     end
 
@@ -17,16 +17,16 @@ describe Book do
       expect(@book.genre[:name]).to eq 'Comedy'
     end
 
-      it 'Returns the instance of class' do
-    expect(@book).to be_instance_of Book
-  end
+    it 'Returns the instance of class' do
+      expect(@book).to be_instance_of Book
+    end
 
-  it 'Return an empty array' do
-    expect(@book.genre[:items]).to eql []
-  end
+    it 'Return an empty array' do
+      expect(@book.genre[:items]).to eql []
+    end
 
-  it 'Return true if cover_state is good' do
-    expect(@book.move_to_archive).to eql false
-  end
+    it 'Return true if cover_state is good' do
+      expect(@book.move_to_archive).to eql false
+    end
   end
 end
